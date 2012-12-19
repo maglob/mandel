7fa8b736e601a7aaafdde0133fc1c476b3bea9a2
Marko Aalto <marko.aalto@iki.fi>
2012-12-19 09:11:29 +0200

    Updated description, and uploaded the code

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ mandel
 ======
 
 ASCII Mandelbrot zoom
+
+Assumes terminal width of 80 chars.
+
+Try with:
+- gcc mandel.c
+- ./a.out
+
---
+++ mandel.c
@@ -0,0 +1,3 @@
+main(){char h[2001]=" Ma+\x1b[H",*k,c;float z=20,D,a,i,s,y;for(;k=h+7,z*=
+.998,z>8e-4;puts(h))for(s=.119+z;(k-h-7)%80<1?y=1.4+z,s-=z/15:k-h-1768;y-=
+z/40,*k++=h[c/4&3])for(c=a=D=0;--c&&a*a+D*D<4;a=a*a-D*D-y,D=2*i*D+s)i=a;}
